bb5582a6cb5d5a6ef723256fdc9e089a520f7b87
Peter Njoroge <njoroge.pitz@gmail.com>
2018-08-29 11:58:26 +0300

    add structure till level 3

--- js/script.js
+++ js/script.js
@@ -4,24 +4,4 @@ $(document).ready(function(){
     $("#question2-0").show();
   });
 
-  $("#quest1-1").click(function(){
-    $("#question1").hide();
-    $("#question2-1").show();
-  });
-
-  $("#quest1-2").click(function(){
-    $("#question1").hide();
-    $("#question2-2").show();
-  });
-
-  $("#quest1-3").click(function(){
-    $("#question1").hide();
-    $("#question2-3").show();
-  });
-
-  $("#quest2-0").click(function(){
-    $("#question1").hide();
-    $("#question2-3").show();
-
-  });
 });
